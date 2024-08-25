2369ae2675727c4aaf7cb0f0c03ef48c12dd7232
= <=>
2024-08-25 11:26:04 +0530

    "Updated ServiceCard component: changed text color conditionally based on loan ID, adjusted button styles and positioning"

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const ServiceCard = (props) => {
     return (
         <>
-            <div className={`service_card  relative w-full h-full p-6 rounded-2xl  ${props.bg} bg-opacity-${props.loan.id * 20}`}>
+            <div className={`service_card ${props.loan.id > 2 ? 'text-white' : 'text-black' }  relative w-full h-full p-6 rounded-2xl  ${props.bg} bg-opacity-${props.loan.id * 20}`}>
                 <div className="w-full mb-10 mt-6">
                     <div className="circle_icon rounded-full p-4  size-20 bg-white text-center grid place-content-center">
                         <img src={props.loan.icon} alt="" width={60} />
@@ -16,8 +16,8 @@ const ServiceCard = (props) => {
                     <h2 className="text-xl">{props.loan.loan}</h2>
                     <p>{props.loan.description}</p>
                 </div>
-                <div className="w-full absolute bottom-5 start-4">
-                    <Link to={'/loan-application'} className="btn bg-white rounded-full px-6 py-3 cursor-pointer">
+                <div className="w-full absolute bottom-10 start-4">
+                    <Link to={'/loan-application'} className="btn bg-white text-black rounded-full px-6 py-3 cursor-pointer">
                         <PlusOutlined />   Apply Now
                     </Link>
                 </div>
